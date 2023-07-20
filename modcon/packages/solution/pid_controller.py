7f0483ea2b8aa4b99534c507f626c06fc8c5f3ee
Simon Alphonse-Cormier <simon.alphonse-cormier.1@ulaval.ca>
2023-07-20 15:43:47 -0400

    fonction pour PID

--- modcon/packages/solution/pid_controller.py
+++ modcon/packages/solution/pid_controller.py
@@ -16,7 +16,7 @@ def PIDController(
     Args:
         v_0:        linear Duckiebot speed (given).
         theta_ref:  reference heading pose.
-        theta_hat:  the current estiamted theta.
+        theta_hat:  the current estimated theta.
         prev_e:     tracking error at previous iteration.
         prev_int:   previous integral error term.
         delta_t:    time interval since last call.
@@ -26,12 +26,17 @@ def PIDController(
         e:       current tracking error (automatically becomes prev_e_y at next iteration).
         e_int:   current integral error (automatically becomes prev_int_y at next iteration).
     """
+    
+    e = theta_hat - theta_ref
+    e_int = prev_int + e*delta_t
+    e_der = (e - prev_e)/delta_t
 
-    # TODO: these are random values, you have to implement your own PID controller in here
-    omega = np.random.uniform(-8.0, 8.0)
-    e = np.random.random()
-    e_int = np.random.random()
-    # Hint: print for debugging
+    Kp = 5
+    Ki = 0.2
+    Kd = 0.1
+
+    omega = Kp*e + Ki*e_int + Kd*e_der
+
     # print(f"\n\nDelta time : {delta_t} \nE : {np.rad2deg(e)} \nE int : {e_int} \nPrev e : {prev_e} \nU : {u} \nTheta hat: {np.rad2deg(theta_hat)} \n")
-    # ---
+
     return v_0, omega, e, e_int
